7d0db1172039e2c808d3f3619bf700fb9c526c58
vannnyle@gmail.com <vannnyle@gmail.com>
2021-09-14 15:19:04 +0300

    feat: add placeholders for videos in FeaturedTalks

--- src/components/pages/home/learn/featured-talks/featured-talks.jsx
+++ src/components/pages/home/learn/featured-talks/featured-talks.jsx
@@ -1,3 +1,5 @@
+import { graphql, useStaticQuery } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import PropTypes from 'prop-types';
 import React from 'react';
 
@@ -9,26 +11,60 @@ import CardSvg from './images/card.inline.svg';
 const title = 'Featured talks';
 
 const videoUrls = [
-  'https://youtu.be/vNuEx0wB_-4',
-  'https://youtu.be/fNtG0iHYne4',
-  'https://youtu.be/u-4naOMfs_w',
-  'https://youtu.be/slBAYUDABDA',
+  { thumbnail: 'card1', url: 'https://youtu.be/vNuEx0wB_-4' },
+  { thumbnail: 'card2', url: 'https://youtu.be/fNtG0iHYne4' },
+  { thumbnail: 'card3', url: 'https://youtu.be/u-4naOMfs_w' },
+  { thumbnail: 'card4', url: 'https://youtu.be/slBAYUDABDA' },
 ];
 
-const FeaturedTalks = ({ className }) => (
-  <div className={className}>
-    <Heading tag="h3" theme="gray">
-      {title}
-    </Heading>
-    <div className="grid grid-cols-1 gap-8 mt-8 lg:mt-10 xs:grid-cols-2">
-      {videoUrls.map((url, index) => (
-        <Link key={index} to={url} target="_blank" rel="noopener noreferrer">
-          <CardSvg className="w-full h-auto" />
-        </Link>
-      ))}
+const FeaturedTalks = ({ className }) => {
+  const { card1, card2, card3, card4 } = useStaticQuery(graphql`
+    query {
+      card1: file(relativePath: { eq: "pages/home/featured-talks/card-1.png" }) {
+        childImageSharp {
+          gatsbyImageData(width: 228)
+        }
+      }
+      card2: file(relativePath: { eq: "pages/home/featured-talks/card-2.png" }) {
+        childImageSharp {
+          gatsbyImageData(width: 228)
+        }
+      }
+      card3: file(relativePath: { eq: "pages/home/featured-talks/card-3.png" }) {
+        childImageSharp {
+          gatsbyImageData(width: 228)
+        }
+      }
+      card4: file(relativePath: { eq: "pages/home/featured-talks/card-4.png" }) {
+        childImageSharp {
+          gatsbyImageData(width: 228)
+        }
+      }
+    }
+  `);
+  const placeholders = { card1, card2, card3, card4 };
+  return (
+    <div className={className}>
+      <Heading tag="h3" theme="gray">
+        {title}
+      </Heading>
+      <div className="grid grid-cols-1 gap-8 mt-8 lg:mt-10 xs:grid-cols-2">
+        {videoUrls.map(({ thumbnail, url }, index) => {
+          const card = placeholders[thumbnail];
+          return (
+            <Link key={index} to={url} target="_blank" rel="noopener noreferrer">
+              <GatsbyImage
+                className="w-full h-auto rounded-lg rounded-image"
+                image={getImage(card)}
+                alt=""
+              />
+            </Link>
+          );
+        })}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 FeaturedTalks.propTypes = {
   className: PropTypes.string,
