1d8c1dcb6c0858ce5ff0ca545042ab7486c8d739
vannnyle@gmail.com <vannnyle@gmail.com>
2021-09-30 18:19:46 +0300

    fix: update content for Enterprise Distributions

--- src/components/pages/learn/enterprise-distributions/enterprise-distributions.jsx
+++ src/components/pages/learn/enterprise-distributions/enterprise-distributions.jsx
@@ -4,12 +4,17 @@ import Container from 'components/shared/container';
 import Heading from 'components/shared/heading';
 import Link from 'components/shared/link';
 import List from 'components/shared/list';
+import IsovalentLogo from 'icons/logo-isovalent.inline.svg';
 
 const title = 'Enterprise Distributions';
 
 const card = {
   title: 'Isovalent',
-  items: ['24/7 Enterprise Support with SLA', 'Hardened, Enterprisegrade, Extended EOL'],
+  items: [
+    '24/7 Enterprise Support with SLA',
+    'Hardened, Enterprisegrade, Extended EOL',
+    'Additional Enterprise Functionality',
+  ],
   linkUrl: '/enterprise',
   linkText: 'Learn more',
 };
@@ -30,10 +35,8 @@ const EnterpriseDistributions = () => (
       <Heading tag="h2">{title}</Heading>
       <div className="grid grid-cols-1 mt-10 gap-y-10 gap-x-8 md:grid-cols-12 lg:mt-14">
         <div className="p-6 border rounded-lg md:col-span-6 lg:col-span-5 lg:p-8 border-gray-3">
-          <Heading size="sm" tag="h3">
-            {card.title}
-          </Heading>
-          <ul className="mt-3 space-y-2">
+          <IsovalentLogo />
+          <ul className="space-y-2 mt-7">
             {card.items.map((item, index) => (
               <li
                 className="text-lg pl-4 relative before:absolute before:w-1.5 before:h-1.5 before:bg-black before:rounded-full before:left-0 before:top-1/2 before:-translate-y-1/2"
