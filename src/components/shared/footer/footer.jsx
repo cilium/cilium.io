f3696eaa4f61b3d951c53888c2626a2f33ae6717
vannnyle@gmail.com <vannnyle@gmail.com>
2021-09-30 18:22:51 +0300

    feat: add text to component Footer

--- src/components/shared/footer/footer.jsx
+++ src/components/shared/footer/footer.jsx
@@ -21,20 +21,53 @@ const navigation = [
 
 const Footer = () => (
   <footer className="mt-20 lg:mt-28 ">
-    <Container className="grid grid-cols-3 py-10 space-y-10 border-t lg:grid-cols-4 gap-x-8 lg:py-14 border-gray-3 lg:space-y-0">
-      <Link to="/">
-        <Logo className="col-span-1 lg:col-span-1" />
-      </Link>
-      <div className="grid grid-cols-1 col-span-3 gap-x-8 gap-y-10 xs:grid-cols-2 sm:grid-cols-3">
-        {navigation.map((menu, index) => (
-          <div className="flex flex-col space-y-6" key={index}>
-            {menu.map(({ name, href }) => (
-              <Link theme="black" type="text" key={name} to={href}>
-                {name}
-              </Link>
-            ))}
-          </div>
-        ))}
+    <Container className="pt-10 pb-10 border-t lg:pt-14 lg:pb-16 border-gray-3">
+      <div className="grid grid-cols-3 space-y-10 lg:grid-cols-4 gap-x-8 lg:space-y-0">
+        <Link to="/">
+          <Logo className="col-span-1 lg:col-span-1" />
+        </Link>
+        <div className="grid grid-cols-1 col-span-3 gap-x-8 gap-y-10 xs:grid-cols-2 sm:grid-cols-3">
+          {navigation.map((menu, index) => (
+            <div className="flex flex-col space-y-6" key={index}>
+              {menu.map(({ name, href }) => (
+                <Link theme="black" type="text" key={name} to={href}>
+                  {name}
+                </Link>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="mt-10 space-y-3 text-sm lg:mt-16 with-link-primary text-gray-1">
+        <p>Copyright © 2021 The Linux Foundation® . All rights reserved.</p>
+        <p>
+          The Linux Foundation has registered trademarks and uses trademarks. For a list of
+          trademarks of The Linux Foundation, please see our{' '}
+          <a
+            href="https://www.linuxfoundation.org/trademark-usage/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Trademark Usage
+          </a>{' '}
+          page. Linux is a registered trademark of Linus Torvalds.{' '}
+          <a
+            href="https://www.linuxfoundation.org/privacy/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Privacy Policy
+          </a>{' '}
+          and{' '}
+          <a
+            href="https://www.linuxfoundation.org/terms/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Terms of Use
+          </a>
+          .
+        </p>
       </div>
     </Container>
   </footer>
