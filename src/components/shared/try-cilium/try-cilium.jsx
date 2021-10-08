6a640c8ac6094a49422b47d097fad1a33ba4f2dd
vannnyle@gmail.com <vannnyle@gmail.com>
2021-10-08 13:21:53 +0300

    fix: remove custom modal in TryCilium

--- src/components/shared/try-cilium/try-cilium.jsx
+++ src/components/shared/try-cilium/try-cilium.jsx
@@ -1,66 +1,72 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import Button from 'components/shared/button';
 import Container from 'components/shared/container';
 import Heading from 'components/shared/heading';
 
-import CustomModal from './custom-modal/custom-modal';
 import GuideSvg from './images/guide.inline.svg';
 import InstallFestSvg from './images/installfest.inline.svg';
 
-const title = 'Try Cilium';
+const title = 'Learn more & try Cilium';
 const items = [
   {
     icon: GuideSvg,
     name: 'Getting Started Guide',
     text: 'Check out the Cilium documentation to quickly get started on a Kubernetes cluster of your choice.',
-    buttonUrl: 'https://docs.cilium.io/en/stable/gettingstarted/k8s-install-default/',
-    buttonText: 'Learn more',
-    buttonTarget: '_blank',
+    buttons: [
+      {
+        buttonUrl: 'https://docs.cilium.io/en/v1.10/',
+        buttonText: 'Learn more',
+        buttonTarget: '_blank',
+      },
+    ],
   },
+
   {
     icon: InstallFestSvg,
     name: 'Weekly InstallFest',
-    text: 'Join us at our weekly InstallFest Meetup, learn and discuss how to setup and get started with Cilium.',
-    buttonUrl: '',
-    buttonText: 'Register',
+    text: 'Join us at our weekly InstallFest and learn how to setup and get started with Cilium.',
+    buttons: [
+      {
+        buttonUrl: 'https://calendly.com/cilium-events/cilium-installfest-emea',
+        buttonText: 'Join Europe',
+        buttonTarget: '_blank',
+      },
+      {
+        buttonUrl: 'https://calendly.com/cilium-events/cilium-installfest-na',
+        buttonText: 'Join Americas',
+        buttonTarget: '_blank',
+      },
+    ],
   },
 ];
 
-const TryCilium = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const handleOpenModal = () => {
-    setIsModalOpen(true);
-  };
-  return (
-    <section className="py-10 mt-12 md:pt-16 md:pb-20 md:mt-20 lg:pt-24 lg:mt-28 lg:pb-28 bg-gray-4">
-      <Container>
-        <Heading tag="h2">{title}</Heading>
-        <div className="grid grid-cols-1 gap-4 mt-6 md:gap-6 lg:gap-8 md:mt-10 md:grid-cols-2 lg:mt-14">
-          {items.map(({ icon: Icon, name, text, buttonUrl, buttonText, buttonTarget }, index) => (
-            <div className="flex flex-col bg-white border rounded-lg border-gray-3" key={index}>
-              <Icon className="w-full h-auto" />
-              <div className="flex flex-col items-center p-6 pb-8 md:px-8 md:pt-6 md:pb-11">
-                <Heading className="!leading-normal text-center" size="sm" tag="h3">
-                  {name}
-                </Heading>
-                <p className="md:text-lg text-center mt-2.5">{text}</p>
-                <Button
-                  className="mt-5"
-                  target={buttonTarget || ''}
-                  to={buttonUrl}
-                  onClick={handleOpenModal}
-                >
-                  {buttonText}
-                </Button>
+const TryCilium = () => (
+  <section className="py-10 mt-12 md:pt-16 md:pb-20 md:mt-20 lg:pt-24 lg:mt-28 lg:pb-28 bg-gray-4">
+    <Container>
+      <Heading tag="h2">{title}</Heading>
+      <div className="grid grid-cols-1 gap-4 mt-6 md:gap-6 lg:gap-8 md:mt-10 md:grid-cols-2 lg:mt-14">
+        {items.map(({ icon: Icon, name, text, buttons }, index) => (
+          <div className="flex flex-col bg-white border rounded-lg border-gray-3" key={index}>
+            <Icon className="w-full h-auto" />
+            <div className="flex flex-col items-center flex-grow p-6 pb-8 md:px-8 md:pt-6 md:pb-11">
+              <Heading className="!leading-normal text-center" size="sm" tag="h3">
+                {name}
+              </Heading>
+              <p className="md:text-lg text-center mt-2.5 mb-5">{text}</p>
+              <div className="flex mt-auto space-x-3 lg:space-x-5">
+                {buttons.map(({ buttonUrl, buttonText, buttonTarget }, index) => (
+                  <Button key={index} target={buttonTarget || ''} to={buttonUrl}>
+                    {buttonText}
+                  </Button>
+                ))}
               </div>
             </div>
-          ))}
-        </div>
-      </Container>
-      <CustomModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
-    </section>
-  );
-};
+          </div>
+        ))}
+      </div>
+    </Container>
+  </section>
+);
 
 export default TryCilium;
