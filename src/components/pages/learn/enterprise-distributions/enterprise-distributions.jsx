84a9fd008eed1d34baf2aa89a13977104458a874
vannnyle@gmail.com <vannnyle@gmail.com>
2021-10-11 19:39:22 +0300

    fix: remove block Resources in EnterpriseDistributions

--- src/components/pages/learn/enterprise-distributions/enterprise-distributions.jsx
+++ src/components/pages/learn/enterprise-distributions/enterprise-distributions.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import Container from 'components/shared/container';
 import Heading from 'components/shared/heading';
 import Link from 'components/shared/link';
-import List from 'components/shared/list';
 import IsovalentLogo from 'icons/logo-isovalent.inline.svg';
 
 const title = 'Enterprise Distributions';
@@ -19,32 +18,6 @@ const card = {
   linkText: 'Learn more',
 };
 
-const resources = {
-  title: 'Resources',
-  items: [
-    {
-      linkUrl: ' https://isovalent.com/blog/post/gartner-cool-vendor',
-      linkTarget: '_blank',
-      linkText: 'Isovalent Named Gartner Cool Vendor',
-    },
-    {
-      linkUrl: 'https://isovalent.com/blog/post/2021-09-aws-eks-anywhere-chooses-cilium',
-      linkTarget: '_blank',
-      linkText: 'AWS picks Cilium for Networking & Security on EKS Anywhere',
-    },
-    {
-      linkUrl:
-        'https://cloud.google.com/blog/products/containers-kubernetes/bringing-ebpf-and-cilium-to-google-kubernetes-engine',
-      linkTarget: '_blank',
-      linkText: 'New GKE Dataplane V2 increases security and visibility for containers',
-    },
-    {
-      linkUrl: 'https://isovalent.com/blog/post/2021-08-ebpf-foundation-announcement',
-      linkTarget: '_blank',
-      linkText: 'Facebook, Google, Isovalent, Microsoft, and Netflix announce eBPF Foundation',
-    },
-  ],
-};
 const EnterpriseDistributions = () => (
   <section className="mt-10 md:mt-20 lg:mt-28">
     <Container>
@@ -66,7 +39,6 @@ const EnterpriseDistributions = () => (
             {card.linkText}
           </Link>
         </div>
-        <List className="md:col-start-7 md:col-end-13 lg:col-end-12" {...resources} />
       </div>
     </Container>
   </section>
