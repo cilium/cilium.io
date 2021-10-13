5c76e057ce76f2cb8910a35c77e26b13d973fa3d
vannnyle@gmail.com <vannnyle@gmail.com>
2021-10-13 12:55:04 +0300

    chore: add metadata text description

--- src/utils/seo-metadata.js
+++ src/utils/seo-metadata.js
@@ -1,10 +1,12 @@
 export default {
   enterprise: {
-    title: 'Cilium - Enterprise Distributions & Training',
-    // description: '',
+    // title: 'Cilium - Enterprise Distributions & Training',
+    description:
+      'Cilium was originally created by Isovalent and contributed to the CNCF as an incubation-level project in 2021',
   },
   learn: {
-    title: 'Cilium - Getting Started',
-    // description: '',
+    // title: 'Cilium - What is Cilium?',
+    description:
+      'Cilium is an open source project to provide networking, security, and observability for cloud native environments such as Kubernetes clusters and other container orchestration platforms.',
   },
 };
