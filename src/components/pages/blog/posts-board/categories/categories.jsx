f779ffd59cbae8651db676aa3e82ad2dda33a3b3
vannnyle@gmail.com <vannnyle@gmail.com>
2021-10-22 12:33:41 +0300

    fix: adjust paddings and space between categories

--- src/components/pages/blog/posts-board/categories/categories.jsx
+++ src/components/pages/blog/posts-board/categories/categories.jsx
@@ -3,20 +3,22 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 const Categories = ({ categories, currentCategory, handleClick }) => (
-  <div className="flex px-4 mt-6 -mx-4 overflow-x-auto gap-x-4 xl:flex-wrap xl:overflow-visible md:px-6 lg:px-10 xl:px-0 md:-mx-6 lg:-mx-10 xl:mx-0 no-scrollbar md:gap-x-6 lg:gap-x-10 md:mt-10 lg:mt-14">
+  <div className="flex px-4 mt-6 -mx-4 overflow-x-auto gap-x-3.5 xl:flex-wrap xl:overflow-visible md:px-6 lg:px-10 xl:px-0 md:-mx-6 lg:-mx-10 xl:mx-0 no-scrollbar lg:gap-x-5 md:mt-10 lg:mt-14">
     {categories.map((category) => {
       const isActiveElement = currentCategory === category;
+      const isCategoryAll = category === '*';
       return (
         <button
           className={classNames(
             'py-2 lg:py-2.5 font-medium whitespace-nowrap',
-            isActiveElement ? 'px-4 md:px-6 lg:px-10 text-white bg-primary-1 rounded' : 'px-0'
+            isActiveElement ? ' text-white bg-primary-1 rounded' : '',
+            isCategoryAll ? 'px-4 md:px-6 lg:px-10' : 'px-3'
           )}
           type="button"
           key={category}
           onClick={(event) => handleClick(event, category)}
         >
-          {category === '*' ? 'All' : category}
+          {isCategoryAll ? 'All' : category}
         </button>
       );
     })}
