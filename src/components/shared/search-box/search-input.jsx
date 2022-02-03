5f2c44cdddf9965e8086fcf6dc011c1849c95454
Thi Van Le <vannnyle@gmail.com>
2022-02-03 18:01:07 +0200

    fix: set cursor pointer for search input

--- src/components/shared/search-box/search-input.jsx
+++ src/components/shared/search-box/search-input.jsx
@@ -9,7 +9,7 @@ export default connectSearchBox(({ refine, currentRefinement, className, onFocus
         'border appearance-none w-full leading-none text-base border-gray-2 py-2.5 pl-9 pr-0 rounded max-h-9 focus-visible:outline-none bg-search-icon bg-no-repeat bg-[center_left_0.625rem] transition-[width,colors]',
         hasFocus
           ? 'sm:w-full pr-2.5 sm:placeholder:text-gray-2'
-          : 'sm:w-9 sm:placeholder:text-transparent',
+          : 'sm:w-9 sm:placeholder:text-transparent cursor-pointer',
         hasFocus && currentRefinement && 'rounded-b-none'
       )}
       aria-label="Search"
