8b7a7a9150f0ea803176b126fadff2058a19a7f7
Thi Van Le <vannnyle@gmail.com>
2022-02-10 16:26:05 +0200

    chore: swap plugin params in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,19 +48,6 @@ module.exports = {
             },
           },
           {
-            resolve: 'gatsby-remark-prismjs',
-            options: {
-              // Class prefix for <pre> tags containing syntax highlighting;
-              // defaults to 'language-' (eg <pre class="language-js">).
-              // If your site loads Prism into the browser at runtime,
-              // (eg for use with libraries like react-live),
-              // you may use this to prevent Prism from re-processing syntax.
-              // This is an uncommon use-case though;
-              // If you're unsure, it's best to use the default value.
-              classPrefix: 'language-',
-            },
-          },
-          {
             resolve: 'gatsby-remark-video',
             options: {
               width: 1008,
@@ -71,6 +58,19 @@ module.exports = {
               playsinline: true,
               controls: true,
               loop: true,
+            },
+          },
+          {
+            resolve: 'gatsby-remark-prismjs',
+            options: {
+              // Class prefix for <pre> tags containing syntax highlighting;
+              // defaults to 'language-' (eg <pre class="language-js">).
+              // If your site loads Prism into the browser at runtime,
+              // (eg for use with libraries like react-live),
+              // you may use this to prevent Prism from re-processing syntax.
+              // This is an uncommon use-case though;
+              // If you're unsure, it's best to use the default value.
+              classPrefix: 'language-',
             },
           },
           'gatsby-remark-responsive-iframe',
