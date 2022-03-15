f1f7b9cee8a5c2af383e9365af49451a706b9203
Thi Van Le <vannnyle@gmail.com>
2022-03-15 11:27:19 +0200

    fix: move content to Cards component

--- src/components/pages/get-help/cards/cards.jsx
+++ src/components/pages/get-help/cards/cards.jsx
@@ -1,4 +1,3 @@
-import PropTypes from 'prop-types';
 import React from 'react';
 
 import Button from 'components/shared/button';
@@ -9,18 +8,39 @@ import GithubIcon from './images/github.inline.svg';
 import SlackIcon from './images/slack.inline.svg';
 import SupportIcon from './images/support.inline.svg';
 
-const icons = {
-  github: GithubIcon,
-  support: SupportIcon,
-  slack: SlackIcon,
-};
+const items = [
+  {
+    icon: GithubIcon,
+    title: 'Slack',
+    description:
+      'For live conversation and quick questions, join the Cilium Slack workspace. Don’t forget to say hi!',
+    buttonText: 'Join Slack',
+    buttonUrl: 'https://cilium.herokuapp.com/',
+    buttonTarget: '_blank',
+  },
+  {
+    icon: SupportIcon,
+    title: 'Training and Support',
+    description: 'Please check the Enterprise page.',
+    buttonText: 'Check it Now',
+    buttonUrl: '/enterprise',
+  },
+  {
+    icon: SlackIcon,
+    title: 'GitHub',
+    description:
+      'Cilium uses GitHub tags to maintain a list of asked questions. So you can check if your question is answered.',
+    buttonText: 'Join GitHub',
+    buttonUrl: 'https://github.com/cilium/cilium',
+    buttonTarget: '_blank',
+  },
+];
 
-const Cards = ({ items }) => (
+const Cards = () => (
   <div className="bg-gray-4 pt-12 pb-28">
     <Container className="grid grid-cols-12 gap-y-8 lg:gap-x-6 xl:gap-x-8" tag="ul">
-      {items.map(({ iconName, title, description, buttonText, buttonUrl, buttonTarget }, index) => {
-        const Icon = icons[iconName];
-        return (
+      {items.map(
+        ({ icon: Icon, title, description, buttonText, buttonUrl, buttonTarget }, index) => (
           <li
             className="flex flex-col md:flex-row lg:flex-col col-span-full lg:col-span-4 space-y-4 md:space-y-0 md:space-x-5 lg:space-x-0 lg:space-y-5 bg-white rounded-xl shadow-card p-8"
             key={index}
@@ -42,23 +62,10 @@ const Cards = ({ items }) => (
               </Button>
             </div>
           </li>
-        );
-      })}
+        )
+      )}
     </Container>
   </div>
 );
-
-Cards.propTypes = {
-  items: PropTypes.arrayOf(
-    PropTypes.exact({
-      iconName: PropTypes.string.isRequired,
-      title: PropTypes.string.isRequired,
-      description: PropTypes.string.isRequired,
-      buttonText: PropTypes.string.isRequired,
-      buttonUrl: PropTypes.string.isRequired,
-      buttonTarget: PropTypes.string,
-    })
-  ).isRequired,
-};
 
 export default Cards;
--- src/pages/get-help.jsx
+++ src/pages/get-help.jsx
@@ -7,38 +7,10 @@ import Community from 'components/shared/community';
 import MainLayout from 'layouts/main/main';
 import { getHelp as seo } from 'utils/seo-metadata';
 
-const items = [
-  {
-    iconName: 'slack',
-    title: 'Slack',
-    description:
-      'For live conversation and quick questions, join the Cilium Slack workspace. Don’t forget to say hi!',
-    buttonText: 'Join Slack',
-    buttonUrl: 'https://cilium.herokuapp.com/',
-    buttonTarget: '_blank',
-  },
-  {
-    iconName: 'support',
-    title: 'Training and Support',
-    description: 'Please check the Enterprise page.',
-    buttonText: 'Check it Now',
-    buttonUrl: '/enterprise',
-  },
-  {
-    iconName: 'github',
-    title: 'GitHub',
-    description:
-      'Cilium uses GitHub tags to maintain a list of asked questions. So you can check if your question is answered.',
-    buttonText: 'Join GitHub',
-    buttonUrl: 'https://github.com/cilium/cilium',
-    buttonTarget: '_blank',
-  },
-];
-
 const GetHelp = () => (
   <MainLayout theme="gray" pageMetadata={seo} footerWithoutTopBorder>
     <Hero />
-    <Cards items={items} />
+    <Cards />
     <Faq />
     <Community theme="gray" isTitleCentered />
   </MainLayout>
