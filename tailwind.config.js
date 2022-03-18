636f2286b8d6afda5f6ebbbf0e29103edce6113a
Andrew Golovanov <spacewebdeveloper@gmail.com>
2022-03-18 14:24:37 +0300

    feat(styles): add a shadow to the cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -46,6 +46,7 @@ module.exports = {
         primary: '0px 5px 20px rgba(0, 0, 0, 0.09)',
         secondary: '0px 10px 20px rgba(0, 0, 0, 0.09)',
         tertiary: '0px 5px 20px rgba(0, 0, 0, 0.1)',
+        card: '0px 2px 10px rgba(20, 26, 31, 0.15)',
       },
       borderRadius: {
         large: '20px',
