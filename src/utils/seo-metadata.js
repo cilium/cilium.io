a0211206a7985e30441a9f94ff688b71133b0952
Andrew Golovanov <spacewebdeveloper@gmail.com>
2022-03-18 14:26:03 +0300

    chore: minor changes

--- src/utils/seo-metadata.js
+++ src/utils/seo-metadata.js
@@ -1,12 +1,19 @@
-export default {
-  enterprise: {
-    // title: 'Cilium - Enterprise Distributions & Training',
-    description:
-      'Cilium was originally created by Isovalent and contributed to the CNCF as an incubation-level project in 2021',
-  },
-  learn: {
-    // title: 'Cilium - What is Cilium?',
-    description:
-      'Cilium is an open source project to provide networking, security, and observability for cloud native environments such as Kubernetes clusters and other container orchestration platforms.',
-  },
+const enterprise = {
+  // title: 'Cilium - Enterprise Distributions & Training',
+  description:
+    'Cilium was originally created by Isovalent and contributed to the CNCF as an incubation-level project in 2021',
 };
+
+const learn = {
+  // title: 'Cilium - What is Cilium?',
+  description:
+    'Cilium is an open source project to provide networking, security, and observability for cloud native environments such as Kubernetes clusters and other container orchestration platforms.',
+};
+
+const tellingStory = {
+  title: 'Cilium - Get help telling your story about Cilium',
+  description:
+    'Are you passionate about Cilium and sharing your experience with the broader community? We want to help you tell your story!',
+};
+
+export { enterprise, learn, tellingStory };
