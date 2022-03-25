b3f8948b821a7aa0bacea15c4def85293c7dd3e6
Thi Van Le <vannnyle@gmail.com>
2022-03-25 14:12:58 +0200

    fix: reduce search box size

--- src/components/shared/search-box/search-input.jsx
+++ src/components/shared/search-box/search-input.jsx
@@ -9,10 +9,10 @@ export default connectSearchBox(({ refine, currentRefinement, className, onFocus
     </label>
     <input
       className={classNames(
-        'max-h-9 w-full appearance-none rounded border border-gray-2 bg-search-icon bg-[center_left_0.625rem] bg-no-repeat py-2.5 pl-9 pr-0 text-base leading-none transition-[width,colors] focus-visible:outline-none',
+        'max-h-8 w-full appearance-none rounded border border-gray-2 bg-search-icon bg-[center_left_0.5rem] bg-no-repeat py-2.5 pl-8 pr-0 text-sm leading-none transition-[width,colors] focus-visible:outline-none',
         hasFocus
           ? 'pr-2.5 sm:w-full sm:placeholder:text-gray-2'
-          : 'cursor-pointer sm:w-9 sm:placeholder:text-transparent',
+          : 'cursor-pointer sm:w-8 sm:placeholder:text-transparent',
         hasFocus && currentRefinement && 'rounded-b-none'
       )}
       aria-label="Search"
