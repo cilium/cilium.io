cdef32674f642fa7da8b68a2785fe8e7b0fca5f4
Thi Van Le <vannnyle@gmail.com>
2022-03-29 17:33:12 +0300

    fix: update burger

--- src/components/shared/header/burger/burger.jsx
+++ src/components/shared/header/burger/burger.jsx
@@ -1,38 +1,102 @@
 import classNames from 'classnames';
+import { motion } from 'framer-motion';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const Burger = ({ className: additionalClassName, onClick, isToggled }) => (
-  <div className={additionalClassName}>
-    <button
-      className={classNames('group relative -mr-1 h-6 w-7 p-1')}
-      type="button"
-      onClick={onClick}
-    >
-      <span
-        className={classNames(
-          'absolute top-1 left-1 block h-0.5 w-5 rounded-sm bg-black transition-all duration-300 ease-in-out group-hover:bg-gray-1',
-          isToggled ? 'invisible w-0 opacity-0' : 'visible opacity-100'
-        )}
-      />
-      <span
-        className={classNames(
-          'absolute left-1 block h-0.5 rounded-sm bg-black transition-all duration-300 ease-in-out group-hover:bg-gray-1',
-          isToggled ? 'top-2.5 w-5 rotate-45' : 'top-2.5 w-3.5 rotate-0'
-        )}
-      />
-      <span
-        className={classNames(
-          'absolute left-1 block h-0.5 rounded-sm bg-black transition-all duration-300 ease-in-out group-hover:bg-gray-1',
-          isToggled ? 'top-2.5 w-5 -rotate-45' : 'top-4 w-5 rotate-0'
-        )}
-      />
-    </button>
-  </div>
+const ANIMATION_DURATION = 0.2;
+
+const Burger = ({ className, theme, isToggled, onClick }) => (
+  <motion.button
+    className={classNames(
+      'relative h-5 w-7',
+      theme === 'white' && 'text-white',
+      theme === 'black' && 'text-black',
+      isToggled && 'text-black',
+      className
+    )}
+    type="button"
+    animate={isToggled ? 'toggled' : 'initial'}
+    onClick={onClick}
+  >
+    <motion.span
+      className="absolute top-0 right-0 block h-0.5 w-full rounded-full bg-current transition-colors duration-200"
+      variants={{
+        initial: {
+          top: 0,
+          display: 'block',
+          transition: { duration: ANIMATION_DURATION, delay: ANIMATION_DURATION },
+        },
+        toggled: {
+          top: 9,
+          transition: { duration: ANIMATION_DURATION },
+          transitionEnd: { display: 'none' },
+        },
+      }}
+    />
+    <motion.span
+      className="absolute top-[9px] left-0 block h-0.5 w-5 rounded-full bg-current transition-colors duration-200"
+      variants={{
+        initial: {
+          display: 'block',
+          transition: { delay: ANIMATION_DURATION },
+        },
+        toggled: {
+          display: 'none',
+          transition: { delay: ANIMATION_DURATION },
+        },
+      }}
+    />
+    <motion.span
+      className="absolute bottom-0 right-0 block h-0.5 w-full rounded-full bg-current transition-colors duration-200"
+      variants={{
+        initial: {
+          bottom: 0,
+          display: 'block',
+          transition: { duration: ANIMATION_DURATION, delay: ANIMATION_DURATION },
+        },
+        toggled: {
+          bottom: 9,
+          transition: { duration: ANIMATION_DURATION },
+          transitionEnd: { display: 'none' },
+        },
+      }}
+    />
+    <motion.span
+      className="absolute top-[9px] right-0 hidden h-0.5 w-full rounded-full bg-current transition-colors duration-200"
+      variants={{
+        initial: {
+          rotate: '0deg',
+          transition: { duration: ANIMATION_DURATION },
+          transitionEnd: { display: 'none' },
+        },
+        toggled: {
+          display: 'block',
+          rotate: '45deg',
+          transition: { duration: ANIMATION_DURATION, delay: ANIMATION_DURATION },
+        },
+      }}
+    />
+    <motion.span
+      className="absolute top-[9px] right-0 hidden h-0.5 w-full rounded-full bg-current transition-colors duration-200"
+      variants={{
+        initial: {
+          rotate: '0deg',
+          transition: { duration: ANIMATION_DURATION },
+          transitionEnd: { display: 'none' },
+        },
+        toggled: {
+          display: 'block',
+          rotate: '-45deg',
+          transition: { duration: ANIMATION_DURATION, delay: ANIMATION_DURATION },
+        },
+      }}
+    />
+  </motion.button>
 );
 
 Burger.propTypes = {
   className: PropTypes.string,
+  theme: PropTypes.oneOf(['white', 'black']).isRequired,
   isToggled: PropTypes.bool,
   onClick: PropTypes.func,
 };
