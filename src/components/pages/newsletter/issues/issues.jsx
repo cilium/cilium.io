59aee4cb029bca3d423de8e4f2227c1210ab7bad
Ekaterina Martynko <ekaterina.martynko@gmail.com>
2022-07-08 14:55:30 +0300

    fix: review and color numbering fixes

--- src/components/pages/newsletter/issues/issues.jsx
+++ src/components/pages/newsletter/issues/issues.jsx
@@ -31,7 +31,7 @@ const items = [
 
 const Issues = () => (
   <div className="bg-white py-10 md:py-20 lg:py-28">
-    <Container className="">
+    <Container>
       {items.map(({ year, issues }, index) => (
         <div className="mt-10 first:mt-0 md:mt-20 lg:mt-24" key={index}>
           <Heading className="flat-breaks xl:flat-none" tag="h3" size="md" asHTML>
@@ -40,7 +40,7 @@ const Issues = () => (
           <div className="mt-8 grid grid-cols-1 gap-6 xs:grid-cols-2 sm:gap-8 md:mt-10 md:grid-cols-3 lg:mt-12 lg:grid-cols-4">
             {issues.map(({ title, date, link }, index) => (
               <Link
-                className="min-w-[180px] rounded-lg border-2 border-gray-7 p-4 sm:min-w-[200px] lg:p-6 xl:min-w-[280px]"
+                className="min-w-[180px] rounded-lg border-2 border-gray-6 p-4 sm:min-w-[200px] lg:p-6 xl:min-w-[280px]"
                 key={index}
                 to={link}
                 target="_blank"
@@ -51,7 +51,7 @@ const Issues = () => (
                 </Heading>
                 <div className="mt-3 flex items-center space-x-2">
                   <CalendarIcon className="h-4 w-4 shrink-0" />
-                  <p>{date}</p>
+                  <span>{date}</span>
                 </div>
               </Link>
             ))}
