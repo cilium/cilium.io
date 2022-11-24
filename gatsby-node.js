8ef27a4c3be2806a7eb2aae4c210ff8b36967653
Thi Van Le <vannnyle@gmail.com>
2022-11-24 11:57:32 +0100

    fix: add try catch

--- gatsby-node.js
+++ gatsby-node.js
@@ -186,18 +186,23 @@ exports.onCreateNode = ({ node, actions }) => {
 
 exports.sourceNodes = async ({ actions: { createNode }, createContentDigest }) => {
   const getObjects = async () => {
-    if (process.env.HUBSPOT_ACCESS_TOKEN) {
-      const hubspotEmails = await fetch(
-        `https://api.hubapi.com/marketing-emails/v1/emails?limit=150&name__icontains=eCHO+news&orderBy=-publish_date`,
-        {
-          method: 'GET',
-          headers: {
-            Authorization: `Bearer ${process.env.HUBSPOT_ACCESS_TOKEN}`,
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-      const hubspotEmailsData = await hubspotEmails.json();
+    if (process.env.NODE_ENV === 'production' && process.env.HUBSPOT_ACCESS_TOKEN) {
+      let hubspotEmailsData;
+      try {
+        const hubspotEmails = await fetch(
+          `https://api.hubapi.com/marketing-emails/v1/emails?limit=150&name__icontains=eCHO+news&orderBy=-publish_date`,
+          {
+            method: 'GET',
+            headers: {
+              Authorization: `Bearer ${process.env.HUBSPOT_ACCESS_TOKEN}`,
+              'Content-Type': 'application/json',
+            },
+          }
+        );
+        hubspotEmailsData = await hubspotEmails.json();
+      } catch (error) {
+        console.log(error);
+      }
 
       return hubspotEmailsData.objects
         .filter(({ isPublished }) => isPublished !== false)
