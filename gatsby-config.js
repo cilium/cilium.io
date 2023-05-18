900dc94d15c8b582b1857349847400b419197f10
Thi Van Le <vannnyle@gmail.com>
2023-05-18 11:26:28 +0200

    chore: uninstall gatsby-plugin-remove-serviceworker

--- gatsby-config.js
+++ gatsby-config.js
@@ -215,7 +215,6 @@ const plugins = [
       sv: 6,
     },
   },
-  'gatsby-plugin-remove-serviceworker',
   // this (optional) plugin enables Progressive Web App + Offline functionality
   // To learn more, visit: https://gatsby.dev/offline
   // `gatsby-plugin-offline`,
