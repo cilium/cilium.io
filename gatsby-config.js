ac43247e0335bf87badcd6b6bc6f35b4b3723767
Le Thi Van <48465000+vannyle@users.noreply.github.com>
2023-05-22 09:44:36 +0100

    chore: remove pwa (#270)
    * chore: remove service worker
    
    * chore: comment short-name
    
    * chore: disable name web manifest
    
    * chore: uninstall gatsby-plugin-remove-serviceworker

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,8 +77,8 @@ const plugins = [
   {
     resolve: 'gatsby-plugin-manifest',
     options: {
-      name: 'gatsby-starter-default',
-      short_name: 'starter',
+      // name: 'cilium.io',
+      // short_name: 'cilium',
       start_url: '/',
       display: 'minimal-ui',
       icon: 'src/images/favicon.png', // This path is relative to the root of the site.
