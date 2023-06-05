305d83bb08339efa55f5485a06530c9197471afa
paularah <paularah.self@gmail.com>
2023-06-05 22:55:48 +0100

    feat: add bgp page

--- src/components/pages/use-cases/cards/use-case-card.jsx
+++ src/components/pages/use-cases/cards/use-case-card.jsx
@@ -6,7 +6,7 @@ import ArrowIcon from 'icons/arrow.inline.svg';
 const customShadow = '0px 4px 4px rgba(0, 0, 0, 0.25)';
 
 const UseCaseCard = ({ heading, testimonials }) => (
-  <div className="mb-12">
+  <div className="my-12">
     <div className="container mx-auto w-10/12">
       <h3 className="mb-8 text-center text-xl font-bold">{heading}</h3>
       <div className="flex flex-col gap-8 md:gap-12">
