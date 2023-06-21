db4fd74c7b1d9cf3bdf561e9dd4b61178e4c4416
Tobias Klauser <tobias@cilium.io>
2023-06-21 16:53:52 +0200

    Fix Karsten's surname in linked talks
    As spelled on the presentation slides.
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- src/pages/use-cases/bgp.jsx
+++ src/pages/use-cases/bgp.jsx
@@ -55,7 +55,7 @@ const testimonials = [
     description:
       "We run all our Kubernetes nodes as routers that advertises Kubernetes pods IP's to our data center network. We run our nodes with 4 network interfaces in 2 pairs (ECMP). 1 pair for frontend application traffic, 1 pair for backend replication/data traffic. This provides us with flexibility, scalability, redundancy and global routable pod IPs.",
     quote: true,
-    person: 'Karsten Nielson - Senior Systems Engineer, Ikea Retail',
+    person: 'Karsten Nielsen - Senior Systems Engineer, Ikea Retail',
   },
 ];
 
