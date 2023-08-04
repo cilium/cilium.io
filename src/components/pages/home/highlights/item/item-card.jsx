7d2813803712033cdfd955e80c43250c5414d2e1
Nikita <105175118+nikvoblikov@users.noreply.github.com>
2023-08-04 17:33:44 +0300

    style: item-card

--- src/components/pages/home/highlights/item/item-card.jsx
+++ src/components/pages/home/highlights/item/item-card.jsx
@@ -8,11 +8,11 @@ import { USE_CASE_BASE_PATH } from 'utils/routes';
 const ItemCard = ({ icon: Icon, title, path }) => (
   <Link
     to={USE_CASE_BASE_PATH + path}
-    className="group flex h-full flex-col rounded-lg border bg-white p-4 transition-all duration-200 hover:shadow-input md:p-6"
+    className="group flex h-full items-center gap-x-6 rounded-lg border-2 border-gray-4 bg-white p-6 transition-all duration-200 hover:border-0 hover:shadow-input"
   >
     <Icon className="h-16 w-16" />
     <Heading
-      className="mt-5 max-w-full !text-lg font-medium leading-normal group-hover:text-primary-1"
+      className="max-w-full !text-lg font-medium leading-normal group-hover:text-primary-1"
       size="3xs"
       tag="h3"
     >
