4063fb5b17ddd436292aa5d28c7a18baf894ec2a
americano98 <mr.nik.bazarov@gmail.com>
2023-10-27 20:45:09 +0400

    feat: hero update

--- src/components/pages/brand/hero/hero.jsx
+++ src/components/pages/brand/hero/hero.jsx
@@ -2,41 +2,42 @@ import React from 'react';
 
 import Button from 'components/shared/button';
 import Container from 'components/shared/container';
-import Heading from 'components/shared/heading';
 
 import backgroundSmSvg from './images/background-sm.svg';
 import backgroundSvg from './images/background.svg';
 
-const title = 'Cilium Brand Guidelines';
+const title = 'Brands Guidelines';
 const description =
-  'Do you want to help grow the buzz around Cilium? We’ve created some guidelines to help you use our brand and assets, including our logo, content, and trademarks';
+  'We’ve created some guidelines to help you use our brands and assets, including our logo, content and trademarks';
 
 const Hero = () => (
-  <section className="relative bg-gray-4 pb-24 pt-20 xs:pb-36 xs:pt-32 sm:pt-10 sm:pb-28 md:pb-36 lg:pt-24 lg:pb-40">
-    <Container className="z-10 flex flex-col text-center sm:text-left lg:text-left">
-      <Heading tag="h1" size="xl">
+  <section className="relative overflow-hidden bg-gray-4 pb-28 pt-20 sm:pb-28 sm:pt-10 lg:pt-[88px] lg:pb-40">
+    <Container className="z-10 flex flex-col items-center text-center">
+      <h1 className="max-w-xs text-4xl font-bold leading-tight sm:max-w-none sm:text-44 lg:text-52">
         {title}
-      </Heading>
-      <div className="mt-7 flex flex-col lg:mx-auto lg:max-w-[708px] lg:self-start xl:ml-80">
-        <p className="text-base md:max-w-[600px] md:text-lg lg:text-xl">{description}</p>
-        <Button
-          to="/data/cilium-brandbook.pdf"
-          theme="primary-1"
-          className="mt-6 !px-7 sm:mt-12 sm:max-w-[257px] sm:self-center md:mt-6 lg:self-start"
-        >
-          Download Brand Guide
-        </Button>
-      </div>
+      </h1>
+      <p className="mt-4 max-w-xs text-base sm:mt-2.5 md:max-w-[560px] md:text-lg">{description}</p>
+      <Button
+        className="mx-auto mt-6 !rounded-md !px-5 sm:mt-7 sm:!px-7"
+        to="/data/cilium-brandbook.pdf"
+        theme="primary-1"
+      >
+        Download Brand Guide
+      </Button>
     </Container>
     <img
-      className="hidden sm:absolute sm:bottom-0 sm:left-1/2 sm:block sm:-translate-x-1/2 sm:translate-y-0 md:bottom-4 xl:-bottom-8"
+      className="absolute -bottom-0.5 left-1/2 hidden w-[1200px] max-w-none -translate-x-1/2 translate-y-0 sm:block lg:w-[1600px]"
       src={backgroundSvg}
       alt=""
+      loading="eager"
+      width={1600}
+      height={554}
       aria-hidden
     />
     <img
-      className="absolute -bottom-[5%] left-1/2 -translate-x-1/2 translate-y-0 xs:-bottom-4 xs:max-w-[390px] sm:hidden"
+      className="absolute -bottom-6 left-1/2 -translate-x-1/2 translate-y-0 xs:-bottom-4 xs:max-w-[390px] sm:hidden"
       src={backgroundSmSvg}
+      loading="eager"
       alt=""
       aria-hidden
     />
