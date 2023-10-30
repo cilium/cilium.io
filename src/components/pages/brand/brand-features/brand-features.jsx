e7918bcc52ea885b98b7d3b20327cc348c606c9d
Nick Bazarov <mr.nik.bazarov@gmail.com>
2023-10-30 17:51:34 +0400

    feat: add Hubble brand (#380)
    * refactor: legal text
    
    * feat: hero update
    
    * feat: add brand features component
    
    * refactor: colors and color map components
    
    * feat: add hubble logos, refactoring
    
    * chore: update page component
    
    * fix: logos, links
    
    * chore: width, height for image, padding
    
    * Update src/components/pages/brand/logos/logos-row/logos-row.jsx
    
    Co-authored-by: Evgenii Sazhin <prosazhin@gmail.com>
    Signed-off-by: Nick Bazarov <mr.nik.bazarov@gmail.com>
    
    ---------
    
    Signed-off-by: Nick Bazarov <mr.nik.bazarov@gmail.com>
    Co-authored-by: Evgenii Sazhin <prosazhin@gmail.com>

---
+++ src/components/pages/brand/brand-features/brand-features.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import Container from 'components/shared/container';
+import Heading from 'components/shared/heading';
+
+import fullLogo from './images/full-logo.svg';
+import icon from './images/icon.svg';
+
+const title = 'Cilium brand';
+const description =
+  'Cilium serves as our primary project. Logo is a crucial part of our brand and one of our most valuable assets. We must ensure its proper usage.';
+
+const items = [
+  {
+    title: 'Primary logo',
+    icon: fullLogo,
+    description:
+      'Our logo is the combination of a simple wordmark and the icon. When using the logo with other logos and graphic elements, please, observe the clean space around the logo to maximize visual effectiveness.',
+  },
+  {
+    title: 'Cillium icon',
+    icon,
+    description:
+      'An icon is a distinct symbol or image that represents the business, used exclusively when a company icon or avatar is needed (typically within a perfect square or circle). In all other instances, use the logo.',
+  },
+];
+
+const BrandFeatures = () => (
+  <section className="mt-16 md:mt-24 lg:mt-32">
+    <Container>
+      <Heading className="text-center" tag="h2" size="lg">
+        {title}
+      </Heading>
+      <p className="mx-auto mt-4 max-w-[632px] text-center text-base leading-relaxed lg:text-lg">
+        {description}
+      </p>
+      <ul className="gap-grid mt-8 grid grid-cols-12 gap-y-6 lg:mt-12">
+        {items.map(({ title, description, icon }, index) => (
+          <li
+            className="col-span-full rounded-xl p-5 pt-2 shadow-[0px_1px_8px_0px_rgba(20,26,31,0.20)] md:col-span-6 lg:px-8 lg:pt-3 lg:pb-8"
+            key={index}
+          >
+            <img
+              className="w-full"
+              src={icon}
+              alt={title}
+              width={528}
+              height={261}
+              loading="lazy"
+            />
+            <h3 className="mt-3 text-xl font-bold leading-snug lg:text-2xl">{title}</h3>
+            <p className="mt-2.5 text-base lg:text-lg lg:leading-relaxed">{description}</p>
+          </li>
+        ))}
+      </ul>
+    </Container>
+  </section>
+);
+
+export default BrandFeatures;
---
+++ src/components/pages/brand/logos/data/logos.js
@@ -0,0 +1,112 @@
+import ciliumDark1 from '../images/cilium-dark-1.svg';
+import ciliumDark2 from '../images/cilium-dark-2.svg';
+import ciliumDark3 from '../images/cilium-dark-3.svg';
+import ciliumDark4 from '../images/cilium-dark-4.svg';
+import ciliumLight1 from '../images/cilium-light-1.svg';
+import ciliumLight2 from '../images/cilium-light-2.svg';
+import ciliumLight3 from '../images/cilium-light-3.svg';
+import ciliumLight4 from '../images/cilium-light-4.svg';
+import hubbleDark1 from '../images/hubble-dark-1.svg';
+import hubbleDark2 from '../images/hubble-dark-2.svg';
+import hubbleDark3 from '../images/hubble-dark-3.svg';
+import hubbleDark4 from '../images/hubble-dark-4.svg';
+import hubbleLight1 from '../images/hubble-light-1.svg';
+import hubbleLight2 from '../images/hubble-light-2.svg';
+import hubbleLight3 from '../images/hubble-light-3.svg';
+import hubbleLight4 from '../images/hubble-light-4.svg';
+
+const ciliumLogos = {
+  dark: [
+    {
+      src: ciliumDark1,
+      width: 191,
+      height: 72,
+    },
+    {
+      src: ciliumDark2,
+      width: 191,
+      height: 72,
+    },
+    {
+      src: ciliumDark3,
+      width: 112,
+      height: 120,
+    },
+    {
+      src: ciliumDark4,
+      width: 112,
+      height: 120,
+    },
+  ],
+  light: [
+    {
+      src: ciliumLight1,
+      width: 191,
+      height: 72,
+    },
+    {
+      src: ciliumLight2,
+      width: 191,
+      height: 72,
+    },
+    {
+      src: ciliumLight3,
+      width: 112,
+      height: 120,
+    },
+    {
+      src: ciliumLight4,
+      width: 112,
+      height: 120,
+    },
+  ],
+};
+
+const hubbleLogos = {
+  dark: [
+    {
+      src: hubbleDark1,
+      width: 151,
+      height: 132,
+    },
+    {
+      src: hubbleDark2,
+      width: 151,
+      height: 132,
+    },
+    {
+      src: hubbleDark3,
+      width: 220,
+      height: 66,
+    },
+    {
+      src: hubbleDark4,
+      width: 220,
+      height: 66,
+    },
+  ],
+  light: [
+    {
+      src: hubbleLight1,
+      width: 142,
+      height: 126,
+    },
+    {
+      src: hubbleLight2,
+      width: 143,
+      height: 126,
+    },
+    {
+      src: hubbleLight3,
+      width: 228,
+      height: 66,
+    },
+    {
+      src: hubbleLight4,
+      width: 228,
+      height: 66,
+    },
+  ],
+};
+
+export { ciliumLogos, hubbleLogos };
