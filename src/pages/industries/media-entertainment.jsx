d7fdfd08be11b9cfaf9afac5c97a70dcf1fb3d58
Bill Mulligan <billmulligan516@gmail.com>
2023-11-30 04:04:49 +0100

    fix: formatting
    Signed-off-by: Bill Mulligan <billmulligan516@gmail.com>

--- src/pages/industries/media-entertainment.jsx
+++ src/pages/industries/media-entertainment.jsx
@@ -95,8 +95,8 @@ const mediaResources = [
     imageSrc: SkyOfficeImage,
     imageAlt: 'sky group  office building',
     title: 'Zero Trust Networking at Scale (20k+ VCPUs, 100+ Dev Teams)',
-    CTAtext: 'Watch the Talk',
-    url: 'https://www.youtube.com/watch?v=1LXKafeuTPo',
+    buttonText: 'Watch the Talk',
+    buttonLink: 'https://www.youtube.com/watch?v=1LXKafeuTPo',
     description:
       'Sky required a performant and secure solution to help them implement zero-trust network security and landed on leveraging additional features in Cilium as the answer.',
   },
