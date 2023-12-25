864e2311ed043e21001958149f9a9a30aa1ef5d6
prosazhin <prosazhin@gmail.com>
2023-12-25 13:32:28 +0300

    feat: fixes logos for use-cases

--- src/components/pages/use-cases/use-case-card/use-case-card.jsx
+++ src/components/pages/use-cases/use-case-card/use-case-card.jsx
@@ -9,7 +9,7 @@ import quotes from './images/quotes.svg';
 
 const UseCaseCard = ({ heading, testimonials }) => (
   <section className="bg-gray-4">
-    <Container className="flex flex-col items-center pb-12 pt-10 md:py-20 lg:py-28">
+    <Container className="flex flex-col items-center pt-10 pb-12 md:py-20 lg:py-28">
       <Heading
         className="mb-12 max-w-full text-center leading-tight lg:mb-14 lg:max-w-[70%] lg:leading-tight xl:leading-tight"
         tag="h2"
@@ -21,10 +21,10 @@ const UseCaseCard = ({ heading, testimonials }) => (
         {testimonials.map(
           ({ logo: Logo, url, CTAtext, description, title, quote, person }, index) => (
             <li
-              className="relative flex flex-col rounded-xl bg-white p-8 shadow-primary"
+              className="relative flex flex-col p-8 bg-white rounded-xl shadow-primary"
               key={title + index}
             >
-              <Logo className="mb-5 h-8 lg:mb-7 lg:h-12" />
+              <Logo className="h-12 mb-5 lg:mb-7" />
               <Heading className="mb-2.5" tag="h3" size="xs">
                 {title}
               </Heading>
@@ -33,7 +33,7 @@ const UseCaseCard = ({ heading, testimonials }) => (
                   <p className="italic">{description}</p>
                   {person && <p className="mt-5 mb-6 font-semibold">{person}</p>}
                   <img
-                    className="absolute right-8 top-7 hidden h-auto w-32 sm:block lg:top-12"
+                    className="absolute hidden w-32 h-auto right-8 top-7 sm:block lg:top-12"
                     src={quotes}
                     width={248}
                     height={180}
@@ -52,7 +52,7 @@ const UseCaseCard = ({ heading, testimonials }) => (
                   rel="noopener noreferrer"
                   type="text"
                   theme="primary"
-                  className="mt-auto border-t border-gray-3 pt-6"
+                  className="pt-6 mt-auto border-t border-gray-3"
                 >
                   {CTAtext}
                 </Link>
