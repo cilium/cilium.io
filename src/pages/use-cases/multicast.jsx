c1059ff6856416448cccfa516e403ce3c30fec77
Paul Arah <paularah.self@gmail.com>
2024-07-24 03:57:54 +0100

    feat: adds multicats usecase

--- src/pages/use-cases/multicast.jsx
+++ src/pages/use-cases/multicast.jsx
@@ -9,6 +9,7 @@ import CiliumLogo from 'icons/logo-cilium.inline.svg';
 import TripLogo from 'icons/logo-trip.inline.svg';
 import CaptaineBee from 'images/pages/usecase/captain-bee.png';
 import MulticastImage1 from 'images/pages/usecase/multicast-1.webp';
+import MulticastImage2 from 'images/pages/usecase/multicast-2.png';
 import MainLayout from 'layouts/main/main';
 
 const heroContent = {
@@ -35,40 +36,22 @@ const sectionContent1 = {
 };
 
 const sectionContent2 = {
-  title: 'Effortless Integration of Existing Network Infrastructure',
+  title: 'Bringing the eBPF revolution to Multicast',
   description:
-    "Cilium's BGP support is designed to be simple and easy to integrate with your existing networking infrastructure. Wherever your application is running, Cilium can ensure efficient BGP routing for all your workloads. Many network infrastructures already use BGP for routing. By leveraging BGP, Cilium can  seamlessly integrate with existing infrastructure, allowing Kubernetes pods to communicate with other parts of the network.",
-  videoSrc: 'https://www.youtube.com/embed/Nzh2jc6qW6Y',
+    "Cilium's multicast implementation is not just efficient, it's also technically advanced. It's based on eBPF, a technology known for its exceptional efficiency and performance. By leveraging eBPF, Cilium ensures that the same level of optimization is brought to multicast as it does to a variety of other networking use cases. ",
+  imageSrc: MulticastImage2,
+  imageWidth: 624,
+  imageHeight: 509,
+  imageRight: false,
+  imageAlt: 'multi-cast ebee',
 };
-
-const testimonials = [
-  {
-    logo: TripLogo,
-    title: "Trip.com's Hybrid Cloud Approach with Cilium for Workload Routing",
-    CTAtext: 'Read The Blog Post',
-    url: 'https://cilium.io/blog/2020/02/05/how-trip-com-uses-cilium/',
-    description:
-      'Trip.com chose a topology based on where workloads run. Bare-metal on-premises workloads use Direct routing via BGP using BIRD, while AWS workloads use AWS ENI routing mode via Cilium AWS ENI support. This topology provides native routing architecture in both the bare-metal and AWS environments with all its performance benefits. In addition, it provides a universal Kubernetes services and Network Policy model across all environments to minimize the differences between the bare-metal and cloud environments.',
-  },
-  {
-    title: 'Leveraging Cilium’s BGP in Private Data Centers',
-    logo: CiliumLogo,
-    CTAtext: 'Watch The Talk',
-    url: 'https://www.youtube.com/watch?v=sg-F_R-ZVNc',
-    description:
-      "We run all our Kubernetes nodes as routers that advertises Kubernetes pods IP's to our data center network. We run our nodes with 4 network interfaces in 2 pairs (ECMP). 1 pair for frontend application traffic, 1 pair for backend replication/data traffic. This provides us with flexibility, scalability, redundancy and global routable pod IPs.",
-    quote: true,
-    person: 'Karsten Nielsen - Senior Systems Engineer, Ikea Retail',
-  },
-];
 
 const BGPPage = () => (
   <MainLayout theme="gray">
     <Hero {...heroContent} />
     <FeatureSection {...sectionContent1} />
     <FeatureSection {...sectionContent2} />
-    <UseCaseCard heading="Who’s using Cilium’s BGP ?" testimonials={testimonials} />
-    <JoinUsCard />
+    <JoinUsCard className="pt-10" />
   </MainLayout>
 );
 
