3118fbd7b2cb5de559b4c1fa93108eac1b9cf853
Anna Artemova <annartmva@gmail.com>
2024-08-19 16:44:01 +0400

    fix: icons and spacing for community links

--- src/components/shared/community/community.jsx
+++ src/components/shared/community/community.jsx
@@ -5,46 +5,43 @@ import React from 'react';
 import Container from 'components/shared/container';
 import Heading from 'components/shared/heading';
 import Link from 'components/shared/link';
-import GithubIcon from 'icons/github.inline.svg';
-import LinkedinIcon from 'icons/linkedin-square.inline.svg';
-import SlackIcon from 'icons/slack.inline.svg';
-import TwitterIcon from 'icons/twitter.inline.svg';
-import YoutubeIcon from 'icons/youtube.inline.svg';
+import githubIcon from 'images/pages/home/community/github.svg';
+import linkedinIcon from 'images/pages/home/community/linkedin.svg';
+import slackIcon from 'images/pages/home/community/slack.svg';
+import xIcon from 'images/pages/home/community/x.svg';
+import youtubeIcon from 'images/pages/home/community/youtube.svg';
 
 const title = 'Community';
 const items = [
   {
-    icon: SlackIcon,
-    logoName: 'Slack',
+    icon: slackIcon,
     title: 'Join our Slack channel',
+    titleWidth: 'xl:w-[122px]',
     url: 'https://slack.cilium.io',
     target: '_blank',
   },
   {
-    icon: GithubIcon,
-    logoName: 'GitHub',
+    icon: githubIcon,
     title: 'Contribute on GitHub',
+    titleWidth: 'xl:w-28',
     url: 'https://github.com/cilium/cilium',
     target: '_blank',
   },
   {
-    icon: TwitterIcon,
-    logoName: 'Twitter',
+    icon: xIcon,
     title: 'Follow us on X',
     titleWidth: 'xl:w-24',
     url: 'https://x.com/ciliumproject',
     target: '_blank',
   },
   {
-    icon: YoutubeIcon,
-    logoName: 'YouTube',
+    icon: youtubeIcon,
     title: 'Watch Echo Livestream',
     url: 'https://www.youtube.com/channel/UCJFUxkVQTBJh3LD1wYBWvuQ',
     target: '_blank',
   },
   {
-    icon: LinkedinIcon,
-    logoName: 'LinkedIn',
+    icon: linkedinIcon,
     title: 'Network with us on LinkedIn',
     titleWidth: 'xl:w-40',
     url: 'https://www.linkedin.com/company/cilium/',
@@ -66,7 +63,7 @@ const themeClassNames = {
 const Community = ({ className, theme, isTitleCentered }) => (
   <section
     className={classNames(
-      'overflow-hidden py-10 sm:overflow-visible md:py-20 lg:py-28',
+      'overflow-hidden py-10 sm:overflow-visible md:py-20 lg:pt-28 lg:pb-32',
       className,
       themeClassNames[theme].wrapper
     )}
@@ -75,20 +72,27 @@ const Community = ({ className, theme, isTitleCentered }) => (
       <Heading className={classNames(isTitleCentered && 'text-center')} tag="h2">
         {title}
       </Heading>
-      <div className="mt-7 grid grid-cols-1 gap-4 sm:grid-cols-2 md:mt-10 md:gap-5 lg:mt-14 lg:grid-cols-3 xl:grid-cols-5">
-        {items.map(({ icon: Icon, logoName, title, url, target, titleWidth }, index) => (
+      <div className="mt-7 grid grid-cols-1 gap-4 sm:grid-cols-2 md:mt-10 md:gap-5 lg:mt-12 lg:grid-cols-3 lg:mb-1.5 xl:grid-cols-5">
+        {items.map(({ icon, title, url, target, titleWidth }, index) => (
           <Link
             to={url}
             target={target}
             className={classNames(
-              'flex items-center rounded-lg p-6 md:flex-col md:pb-8 md:pt-7 lg:pb-10 xl:px-7',
+              'flex items-center rounded-lg p-6 md:flex-col md:pb-8 md:pt-7 lg:pt-9 lg:pb-11 xl:px-7',
               themeClassNames[theme].card
             )}
             key={index}
             type="text"
             theme="black"
           >
-            <Icon className="shrink-0 h-9 w-9 md:h-11 md:w-11" aria-label={`${logoName} logo`} />
+            <img
+              className="shrink-0 h-9 w-9 md:h-11 md:w-11"
+              src={icon}
+              alt=""
+              width="36"
+              height="36"
+              loading="lazy"
+            />
             <span
               className={classNames(
                 'ml-4 text-center font-semibold leading-snug truncate md:m-0 md:mt-[18px] md:text-lg md:leading-snug lg:whitespace-normal xl:leading-tight',
