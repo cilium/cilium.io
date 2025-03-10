db67d34c237fa2fdaeacc28780591a3eee20a59e
Anna Artemova <annartmva@gmail.com>
2025-03-10 18:21:24 +0400

    feat: update mobile version of top banner

--- src/components/shared/top-banner/top-banner.jsx
+++ src/components/shared/top-banner/top-banner.jsx
@@ -12,16 +12,16 @@ const TopBanner = ({ text, url }) => {
   return (
     <section className="top-banner w-full">
       <Link
-        className="group flex gap-3.5 w-full justify-center p-2 leading-normal bg-[#FFF5D6] border-b border-[#FFE391] overflow-hidden sm:py-3 sm:px-8"
+        className="group flex gap-3.5 w-full justify-center items-center px-5 py-3.5 leading-normal bg-[#FFF5D6] border-b border-[#FFE391] overflow-hidden"
         theme="black-primary"
         type="text"
         to={url}
       >
         <span className="truncate !font-normal">{text}</span>
-        <span className="hidden sm:flex items-center gap-1 border-l border-current pl-3.5 font-medium">
-          <span className="tracking-tight whitespace-nowrap">Learn more</span>
-          <ArrowIcon className="ml-2.5 text-center shrink-0" />
+        <span className="hidden sm:block border-l border-current pl-3.5 font-medium tracking-tight whitespace-nowrap">
+          Learn more
         </span>
+        <ArrowIcon className="-ml-1.5 text-center shrink-0" />
       </Link>
     </section>
   );
