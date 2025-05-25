dde48cf8018abe8435cc4a7787f2302f0162f72d
Sandip Kumar Dey <sandipkumardey615@gmail.com>
2025-05-25 21:54:51 +0530

    fix(hooks): move event handler inside useEffect for linter compliance

--- src/hooks/use-click-outside.js
+++ src/hooks/use-click-outside.js
@@ -2,17 +2,17 @@ import { useEffect } from 'react';
 
 const events = [`mousedown`, `touchstart`];
 export default (refs, onClickOutside) => {
-  const isOutside = (element) =>
-    refs.every((ref) => !ref.current || !ref.current.contains(element));
-  const onClick = (event) => {
-    if (isOutside(event.target)) {
-      onClickOutside();
-    }
-  };
   useEffect(() => {
+    const isOutside = (element) =>
+      refs.every((ref) => !ref.current || !ref.current.contains(element));
+    const onClick = (event) => {
+      if (isOutside(event.target)) {
+        onClickOutside();
+      }
+    };
     events.forEach((event) => document.addEventListener(event, onClick));
     return () => {
       events.forEach((event) => document.removeEventListener(event, onClick));
     };
-  });
+  }, [refs, onClickOutside]);
 };
