642caa99288bd7c0641bfc74ab68e15e347763c5
Paul Arah <paularah.self@gmail.com>
2025-07-09 03:22:09 +0100

    feat: add bluesky share button
    Signed-off-by: Paul Arah <paularah.self@gmail.com>

--- src/components/pages/blog-post/content/social-share/social-share.jsx
+++ src/components/pages/blog-post/content/social-share/social-share.jsx
@@ -1,10 +1,11 @@
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { LinkedinShareButton, TwitterShareButton } from 'react-share';
+import { LinkedinShareButton } from 'react-share';
 
+import Button from 'components/shared/button';
+import BlueskyIcon from 'icons/bluesky.inline.svg';
 import LinkedinIcon from 'icons/linkedin.inline.svg';
-import TwitterIcon from 'icons/twitter.inline.svg';
 
 const SocialShare = ({ className, postUrl, title, summary, tags }) => (
   <div
@@ -13,16 +14,21 @@ const SocialShare = ({ className, postUrl, title, summary, tags }) => (
       'flex md:flex-col self-start mt-10 space-x-4 md:space-x-0 md:mt-[calc(100vh/3)] md:space-y-4'
     )}
   >
-    <TwitterShareButton url={postUrl} title={title} via="ciliumproject" hashtags={tags}>
-      <div className="flex items-center justify-center w-12 h-12 transition-colors duration-200 bg-white border rounded-full lg:w-14 lg:h-14 border-gray-3 hover:bg-gray-4">
-        <TwitterIcon />
-      </div>
-    </TwitterShareButton>
     <LinkedinShareButton url={postUrl} title={title} summary={summary}>
       <div className="flex items-center justify-center w-12 h-12 transition-colors duration-200 bg-white border rounded-full lg:w-14 lg:h-14 border-gray-3 hover:bg-gray-4">
         <LinkedinIcon />
       </div>
     </LinkedinShareButton>
+
+    <Button
+      to="https://bsky.app/profile/your-profile"
+      className="group flex items-center justify-center w-12 h-12 text-[#0085FF] hover:text-gray-600 transition-colors duration-200 bg-white border rounded-full lg:w-14 lg:h-14 border-gray-3 hover:bg-gray-4"
+      target="_blank"
+      rel="noopener noreferrer"
+      asDefaultLink
+    >
+      <BlueskyIcon className="w-6 h-6" />
+    </Button>
   </div>
 );
 
