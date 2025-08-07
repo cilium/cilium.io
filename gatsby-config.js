c7ef83b9491d4dc17aa88be66a2e55def4b7d50a
Paul Arah <paularah.self@gmail.com>
2025-08-07 15:28:45 +0100

    fix: fix failing netlify build
    Signed-off-by: Paul Arah <paularah.self@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,290 +1,101 @@
-// Gatsby has dotenv by default
-// eslint-disable-next-line import/no-extraneous-dependencies
-require('dotenv').config();
-const queries = require('./src/utils/algolia-queries');
+const path = require('path');
+
+const { netlifyPlugin } = require('gatsby-adapter-netlify');
 
 const plugins = [
   {
-    resolve: 'gatsby-source-filesystem',
+    resolve: 'gatsby-plugin-mdx',
     options: {
-      name: 'images',
-      path: `${__dirname}/src/images`,
-    },
-  },
-  {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `posts`,
-      path: `${__dirname}/src/posts`,
-    },
-  },
-  {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `events`,
-      path: `${__dirname}/content/events`,
-    },
-  },
-  {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `labs`,
-      path: `${__dirname}/content/labs`,
-    },
-  },
-  {
-    resolve: `gatsby-plugin-mdx`,
-    options: {
-      extensions: [`.mdx`, `.md`],
-      mdxOptions: {
-        remarkPlugins: [
-          // Add GitHub Flavored Markdown (GFM) support
-          // eslint-disable-next-line global-require
-          require('remark-gfm'),
-        ],
-      },
       gatsbyRemarkPlugins: [
-        'gatsby-remark-copy-linked-files',
         {
-          resolve: 'gatsby-remark-images',
+          resolve: 'gatsby-remark-autolink-headers',
           options: {
-            maxWidth: 1008,
-            quality: 90,
-            withWebp: true,
-            backgroundColor: 'white',
-            disableBgImageOnAlpha: true,
-          },
-        },
-        {
-          resolve: 'gatsby-remark-video',
-          options: {
-            width: 1008,
-            height: 'auto',
-            preload: 'auto',
-            muted: true,
-            autoplay: true,
-            playsinline: true,
-            controls: true,
-            loop: true,
+            offsetY: 100,
+            icon: false,
+            className: 'anchor',
           },
         },
         {
           resolve: 'gatsby-remark-prismjs',
           options: {
-            // Class prefix for <pre> tags containing syntax highlighting;
-            // defaults to 'language-' (eg <pre class="language-js">).
-            // If your site loads Prism into the browser at runtime,
-            // (eg for use with libraries like react-live),
-            // you may use this to prevent Prism from re-processing syntax.
-            // This is an uncommon use-case though;
-            // If you're unsure, it's best to use the default value.
-            classPrefix: 'language-',
+            showLineNumbers: true,
           },
         },
-        'gatsby-remark-responsive-iframe',
       ],
     },
   },
-  'gatsby-transformer-sharp',
-  {
-    resolve: 'gatsby-plugin-sharp',
-    options: {
-      defaultQuality: 85,
-      defaults: {
-        placeholder: 'none',
-      },
-    },
-  },
+  'gatsby-plugin-postcss',
+  'gatsby-plugin-cname',
+  'gatsby-plugin-sitemap',
   {
     resolve: 'gatsby-plugin-manifest',
     options: {
-      // name: 'cilium.io',
-      // short_name: 'cilium',
+      name: 'Cilium',
+      short_name: 'Cilium',
       start_url: '/',
+      background_color: '#ffffff',
+      theme_color: '#ffffff',
       display: 'minimal-ui',
-      icon: 'src/images/favicon.png', // This path is relative to the root of the site.
+      icon: 'src/images/favicon.png',
     },
   },
   {
-    resolve: 'gatsby-plugin-svgr-svgo',
+    resolve: 'gatsby-plugin-alias-imports',
     options: {
-      inlineSvgOptions: [
-        {
-          test: /\.inline.svg$/,
-          svgoConfig: {
-            plugins: [
-              {
-                name: 'preset-default',
-                params: {
-                  overrides: {
-                    removeViewBox: false,
-                  },
-                },
-              },
-              'prefixIds',
-            ],
-          },
-        },
-      ],
-      urlSvgOptions: [
-        {
-          test: /\.svg$/,
-          svgoConfig: {
-            plugins: [
-              {
-                name: 'preset-default',
-                params: {
-                  overrides: {
-                    removeViewBox: false,
-                  },
-                },
-              },
-              'prefixIds',
-            ],
-          },
-        },
-      ],
+      alias: {
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@images': path.resolve(__dirname, 'src/images'),
+        '@layouts': path.resolve(__dirname, 'src/layouts'),
+        '@pages': path.resolve(__dirname, 'src/pages'),
+        '@styles': path.resolve(__dirname, 'src/styles'),
+        '@utils': path.resolve(__dirname, 'src/utils'),
+      },
+      extensions: ['js'],
     },
   },
-  'gatsby-alias-imports',
-  'gatsby-plugin-postcss',
-  'gatsby-plugin-sitemap',
-  'gatsby-plugin-image',
   {
-    resolve: 'gatsby-plugin-netlify',
+    resolve: 'gatsby-plugin-react-svg',
     options: {
-      headers: {
-        '/fonts/*': ['Cache-Control: public, max-age=31536000, immutable'],
-        '/*': ['Content-Security-Policy: frame-ancestors instruqt.com play.instruqt.com'],
+      rule: {
+        include: /images/,
       },
     },
   },
   {
-    resolve: `gatsby-plugin-feed`,
+    resolve: 'gatsby-plugin-google-tagmanager',
     options: {
-      query: `
-        {
-          site {
-            siteMetadata {
-              title
-              description
-              siteUrl
-              site_url: siteUrl
-            }
-          }
-        }
-      `,
-      feeds: [
-        {
-          serialize: ({ query: { site, allMdx } }) =>
-            allMdx.nodes.map(({ excerpt, frontmatter }) => {
-              const { path, date, externalUrl } = frontmatter;
-              const url = externalUrl || site.siteMetadata.siteUrl + path;
-              return {
-                ...frontmatter,
-                description: excerpt,
-                date,
-                url,
-                guid: url,
-              };
-            }),
-          query: `
-            {
-              allMdx(
-                filter: {internal: { contentFilePath: { regex: "/posts/" } }, fields: {draft: {eq: false}} }
-                limit: 20
-                sort: { frontmatter: { date: DESC } },
-              ) {
-                nodes {
-                  excerpt
-                  frontmatter {
-                    title
-                    date
-                    path
-                    externalUrl
-                  }
-                }
-              }
-            }
-          `,
-          output: '/blog/rss.xml',
-          title:
-            'Cilium - The latest articles covering eBPF-based Networking, Observability, and Security',
-        },
-        {
-          serialize: ({ query: { allHubspotEmail } }) =>
-            allHubspotEmail.nodes.map(({ name, publishDate, publishedUrl }) => {
-              const date = new Date(Number(publishDate)).toUTCString();
-              return {
-                title: name,
-                date,
-                url: publishedUrl,
-                guid: publishedUrl,
-              };
-            }),
-          query: `
-          {
-            allHubspotEmail(sort: { publishDate: DESC }) {
-              nodes {
-                name
-                publishDate
-                publishedUrl
-              }
-            }
-          }
-          `,
-          output: '/newsletter/rss.xml',
-          title: 'Cilium Newsletter - bi-weekly wrap up of all things eBPF and Cilium',
-        },
-      ],
+      id: 'GTM-TWNN8FF',
+      includeInDevelopment: false,
+      defaultDataLayer: { platform: 'gatsby' },
+    },
+  },
+  {
+    resolve: 'gatsby-plugin-plausible',
+    options: {
+      domain: 'ebpf.foundation',
     },
   },
   {
     resolve: 'gatsby-plugin-canonical-urls',
     options: {
-      siteUrl: process.env.GATSBY_DEFAULT_SITE_URL,
+      siteUrl: 'https://ebpf.foundation',
+      stripQueryString: true,
     },
   },
-  {
-    resolve: `gatsby-plugin-hotjar`,
-    options: {
-      includeInDevelopment: true, // optional parameter to include script in development
-      id: 2658928,
-      sv: 6,
-    },
-  },
-  // this (optional) plugin enables Progressive Web App + Offline functionality
-  // To learn more, visit: https://gatsby.dev/offline
-  // `gatsby-plugin-offline`,
 ];
-
-if (process.env.CONTEXT === 'production') {
-  plugins.push({
-    resolve: `gatsby-plugin-algolia-search`,
-    options: {
-      appId: process.env.GATSBY_ALGOLIA_APP_ID,
-      apiKey: process.env.ALGOLIA_ADMIN_KEY,
-      indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
-      enablePartialUpdates: true,
-      queries,
-      matchFields: ['title', 'excerpt'],
-      chunkSize: 10000, // default: 1000
-    },
-  });
-}
 
 module.exports = {
   trailingSlash: 'always',
+  adapter: netlifyPlugin(),
   siteMetadata: {
-    siteTitle: 'Cilium - Cloud Native, eBPF-based Networking, Observability, and Security', // <title>
-    siteDescription: 'Cloud Native, eBPF-based Networking, Observability, and Security',
-    // pathPrefix: "",
-    siteImage: '/images/social-preview.jpg',
-    siteLanguage: 'en',
-    siteUrl: process.env.GATSBY_DEFAULT_SITE_URL,
-    /* author */
-    authorName: 'cilium',
-    authorTwitterAccount: '@ciliumproject',
+    siteTitle: 'Cilium - Cloud Native, eBPF-based Networking, Observability, and Security',
+    siteTitleAlt: 'Cilium',
+    siteHeadline: 'Cilium - eBPF-based Networking, Observability, and Security',
+    siteUrl: 'https://ebpf.foundation',
+    siteDescription:
+      'Cilium is an open source, eBPF-based software for networking, security, and observability.',
+    siteImage: '/social/banner.jpg',
+    author: '@ebpf',
   },
   plugins,
 };
