4d3f2cd50b75d084784c93dbca578c8ad24c17a8
Ankit Matth <146843890+Ankit-Matth@users.noreply.github.com>
2025-08-18 16:17:23 +0530

    Improve labs banner UI (#712)
    Signed-off-by: Ankit Matth <ankitmatth111@gmail.com>
    Signed-off-by: Oluchi Nwenyi <oluchinwenyi@gmail.com>

--- src/components/pages/labs/banner/banner.jsx
+++ src/components/pages/labs/banner/banner.jsx
@@ -4,7 +4,7 @@ import Button from 'components/shared/button';
 import Heading from 'components/shared/heading';
 
 const Banner = () => (
-  <div className="mt-10 flex w-full flex-col items-center justify-center space-y-6 rounded-xl bg-dark-blue text-white dark:bg-light-gray dark:text-black px-10 py-14 md:mt-16 lg:mt-24 lg:flex-row lg:space-x-8 lg:space-y-0 lg:px-4 xl:mt-32 xl:space-x-14">
+  <div className="mt-10 flex w-full flex-col items-center justify-center space-y-6 rounded-xl bg-white dark:bg-gray-2 px-10 py-14 md:mt-16 lg:mt-24 lg:flex-row lg:space-x-8 lg:space-y-0 lg:px-4 xl:mt-32 xl:space-x-20 shadow-primary">
     <Heading tag="h2" size="sm">
       Want to add your lab to the list? Submit a PR here
     </Heading>
