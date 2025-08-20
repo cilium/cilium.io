5e99a7393819a65a353b4202fe74c41c840e5c78
ADITYATIWARI342005 <142050150+ADITYATIWARI342005@users.noreply.github.com>
2025-08-21 00:22:42 +0530

    Make changes as from review and inspired from other sections on the page
    Signed-off-by: ADITYATIWARI342005 <142050150+ADITYATIWARI342005@users.noreply.github.com>

--- src/components/pages/get-involved/calendar-integration/calendar-integration.jsx
+++ src/components/pages/get-involved/calendar-integration/calendar-integration.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 
+import Button from 'components/shared/button';
 import Container from 'components/shared/container';
 import Heading from 'components/shared/heading';
 
@@ -32,11 +33,11 @@ const CalendarIntegration = () => {
         </div>
 
         <div className="mt-6 text-center">
-          <a
-            href="https://zoom-lfx.platform.linuxfoundation.org/meetings/cilium?view=week"
+          <Button
+            theme="primary-1"
+            to="https://zoom-lfx.platform.linuxfoundation.org/meetings/cilium?view=week"
             target="_blank"
             rel="noopener noreferrer"
-            className="inline-flex items-center px-4 py-2 text-white transition-colors duration-200 bg-blue-600 rounded-lg hover:bg-blue-700"
           >
             Open Calendar in New Tab
             <svg className="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
@@ -47,7 +48,7 @@ const CalendarIntegration = () => {
                 d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
               />
             </svg>
-          </a>
+          </Button>
         </div>
       </Container>
     </section>
