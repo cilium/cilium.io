2f1a16cdcf28fbff9abeed293e2fa4c1052ee4dc
Peace Sandy <78281826+Peacesandy@users.noreply.github.com>
2025-11-05 17:54:09 +0100

    feat: add Organization schema markup for enhanced SEO and structured data (#815)
    * feat: add Organization schema markup to HTML file for improved SEO and brand visibility
    
    Signed-off-by: Peace Sandy <peacesandy04@gmail.com>
    
    * refactor: move orgSchema to module scope for better performance
    
    Signed-off-by: Peace Sandy <peacesandy04@gmail.com>
    
    ---------
    
    Signed-off-by: Peace Sandy <peacesandy04@gmail.com>

--- src/html.js
+++ src/html.js
@@ -6,6 +6,29 @@ const fontsBasePath = '/fonts';
 
 const fontsPaths = ['/inter-regular.woff2', '/inter-bold.woff2'];
 
+const orgSchema = {
+  '@context': 'https://schema.org',
+  '@type': 'Organization',
+  name: 'Cilium',
+  url: 'https://cilium.io',
+  logo: 'https://cilium.io/logo.svg',
+  description:
+    'Cilium is an open source, cloud native solution for providing, securing, and observing network connectivity between workloads, fueled by the revolutionary Kernel technology eBPF',
+  foundingDate: '2015-12',
+  sameAs: [
+    'https://github.com/cilium',
+    'https://twitter.com/ciliumproject',
+    'https://www.linkedin.com/company/cilium',
+    'https://slack.cilium.io',
+    'https://www.youtube.com/channel/UCJFUxkVQTBJh3LD1wYBWvuQ',
+  ],
+  memberOf: {
+    '@type': 'Organization',
+    name: 'Cloud Native Computing Foundation',
+    url: 'https://www.cncf.io',
+  },
+};
+
 export default function HTML(props) {
   return (
     <html {...props.htmlAttributes} lang="en">
@@ -27,6 +50,10 @@ export default function HTML(props) {
           />
         ))}
         {props.headComponents}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(orgSchema) }}
+        />
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
