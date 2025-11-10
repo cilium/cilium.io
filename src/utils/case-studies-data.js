96ff437c58a54d870df2cc9ce0b80217e67c8275
ADITYA TIWARI <142050150+ADITYATIWARI342005@users.noreply.github.com>
2025-11-10 09:04:38 +0530

    Add ESnet to adopters page (#817)
    * docs: add ESnet to adopters page
    
    Signed-off-by: ADITYA TIWARI <142050150+ADITYATIWARI342005@users.noreply.github.com>
    
    * docs: add ESnet logo
    
    Signed-off-by: ADITYA TIWARI <142050150+ADITYATIWARI342005@users.noreply.github.com>
    
    * docs: add logo to user-community
    
    Signed-off-by: ADITYA TIWARI <142050150+ADITYATIWARI342005@users.noreply.github.com>
    
    * fix: add esnet to user community.jsx
    
    Signed-off-by: ADITYA TIWARI <142050150+ADITYATIWARI342005@users.noreply.github.com>
    
    * fix: svg resize for proper rendering
    
    Signed-off-by: ADITYA TIWARI <142050150+ADITYATIWARI342005@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: ADITYA TIWARI <142050150+ADITYATIWARI342005@users.noreply.github.com>

--- src/utils/case-studies-data.js
+++ src/utils/case-studies-data.js
@@ -1133,6 +1133,17 @@ export default {
       },
     ],
   },
+  esnet: {
+    iconName: 'esnet',
+    text: '<b>ESnet</b> uses Cilium for IPv6-first Kubernetes networking for scientific computing infrastructure',
+    links: [
+      {
+        linkText: 'Read Case Study',
+        linkUrl: 'https://www.cncf.io/case-studies/esnet/',
+        linkTarget: '_blank',
+      },
+    ],
+  },
   gdata: {
     iconName: 'gdata',
     text: '<b>G Data</b> uses Cilium in their Kubernetes clusters for networking, security and observability',
