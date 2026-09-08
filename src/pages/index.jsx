32832999fdc497787677ec76b7ba18c5dc5d4a37
Paul Arah <paularah.self@gmail.com>
2026-09-08 11:45:17 +0100

    fix: remove inviter component
    Signed-off-by: Paul Arah <paularah.self@gmail.com>

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,7 +9,6 @@ import Learn from 'components/pages/home/learn';
 import CNCFBanner from 'components/shared/cncf-banner';
 import Community from 'components/shared/community';
 import HandsOn from 'components/shared/hands-on';
-import CommunityInviter from 'components/shared/inviter';
 import SEO from 'components/shared/seo';
 import UserCommunity from 'components/shared/user-community';
 import MainLayout from 'layouts/main';
@@ -87,7 +86,6 @@ const IndexPage = () => (
     <FeaturedTalks talks={talks} />
     <Learn />
     <Community className="pt-10 md:pt-20 lg:pt-32" isTitleCentered />
-    <CommunityInviter />
   </MainLayout>
 );
 
